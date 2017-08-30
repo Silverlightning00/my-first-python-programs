bc04b422e17a76d36cd6cb02731931f95444e70f
Silverlightning00 <slee4848@greenvilleschools.us>
2017-08-30 09:32:52 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+# This program says hello and greets a person by name.
+# Stephen Lee P1
+# August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("Where were you born, " + name + "?")
+place = input()
+print("That's interesting. I'd like to visit " + place + " one day.")
+
---
+++ madlib.py
@@ -0,0 +1,69 @@
+# THE FOREST - Mad Lib
+# Stephen Lee P1
+
+print("This is Stephen's Mad Lib")
+print("Type in words that transform into a story!")
+print("")
+
+print("Adjective")
+adjective1 = input()
+
+print("Name 1:")
+name1 = input()
+print("Name 2:")
+name2 = input()
+print("Noun:")
+noun1 = input()
+print("Noun:")
+noun2 = input()
+print("Noun:")
+noun3 = input()
+print("Noun:")
+noun4 = input()
+print("Verb (ing):")
+verb1 = input()
+print("Noun:")
+noun5 = input()
+print("Noun:")
+noun6 = input()
+print("Verb (ing):")
+verb2 = input()
+print("Noun:")
+noun7 = input()
+print("Verb (ed):")
+verb3 = input()
+print("Noun:")
+noun8 = input()
+print("Noun:")
+noun9 = input()
+print("Adjective:")
+adjective2 = input()
+print("Adjective:")
+adjective3 = input()
+
+# The Story
+
+print("The " + adjective1 + " Forest")
+print(name1 + " and " + name2 + " wanted to go camping. ")
+print("First, they needed a tent. They decided to make a tent out of " +
+      noun1 + " and " + noun2 + ". ")
+print("Next, they packed other things they would need for camping like " +
+      noun3 + " and " + noun4 + ". ")
+print("They were ready to go! They started " + verb1 + " into the woods. ")
+print("They got nervous when they saw a " + noun5 + ". Then they realized" +
+      " it was just a silly " + noun6 + ". ")
+print("When they found a campsite, they " + verb2 + " the " + noun7 +
+      " and then decided to go " + verb3 + ". ")
+print("Now they were tired and tried and asked their parents to help them " +
+      "make a campfire. ")
+print("They used " + noun8 + " and " + noun9 + " to build a fire. ")
+print("It was very " + adjective2 + ". ")
+print("They sat around the campfire telling " + adjective3 + " stories. " +
+      "Finally it was time to go to sleep. They had a great camping trip!")
+
+      
+      
+
+
+      
+
